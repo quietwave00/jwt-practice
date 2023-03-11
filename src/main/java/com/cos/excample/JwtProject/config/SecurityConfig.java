f17f45c002ce8453a68b3b0b8fdbf2f6b509a41a
갸갸 <quietwave00@gmail.com>
2023-03-11 22:59:02 +0900

    필터, 필터 configuration

--- src/main/java/com/cos/excample/JwtProject/config/SecurityConfig.java
+++ src/main/java/com/cos/excample/JwtProject/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.cos.excample.JwtProject.config;
 
+import com.cos.excample.JwtProject.filter.MyFilter1;
+import com.cos.excample.JwtProject.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class); //여기 필터가 먼저 실행됨
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션 사용하지 않겠다
                 .and()
