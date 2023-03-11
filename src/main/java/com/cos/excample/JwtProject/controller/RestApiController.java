d925ddb4b31f9ae2b6baa57065037e461c8d3de1
갸갸 <quietwave00@gmail.com>
2023-03-12 00:22:34 +0900

    회원가입 Controller 추가

--- src/main/java/com/cos/excample/JwtProject/controller/RestApiController.java
+++ src/main/java/com/cos/excample/JwtProject/controller/RestApiController.java
@@ -1,11 +1,20 @@
 package com.cos.excample.JwtProject.controller;
 
+import com.cos.excample.JwtProject.model.User;
+import com.cos.excample.JwtProject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class RestApiController {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
 
     @GetMapping("home")
     public String home() {
@@ -15,6 +24,15 @@ public class RestApiController {
     @PostMapping("token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+
+        return "회원가입 완료";
     }
 
 
