0d8b34b446375fa0e3ab58849216e330fbbac38b
갸갸 <quietwave00@gmail.com>
2023-03-11 18:19:09 +0900

    초기 세팅

---
+++ src/test/java/com/cos/excample/JwtProject/JwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.excample.JwtProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
